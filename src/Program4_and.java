a69f5acbb140a12032764334b284d831d9ce05f1
Ashish <ashishdhomne@hotmail.com>
2022-12-03 14:02:05 +0530

    Program 4

---
+++ src/Program4_and.java
@@ -0,0 +1,18 @@
+public class Program4_and {
+        public static void main(String[] args) {
+
+            int topScore = 80;
+            if (topScore < 100) {
+                System.out.println("You got the high score!");
+            }
+
+            int secondTopScore = 85;
+            if (topScore < secondTopScore)
+            {
+                System.out.println("Greater than second top score and less than 100");
+            }
+
+        }
+    }
+
+
