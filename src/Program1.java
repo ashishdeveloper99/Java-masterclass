d0c5a4ec692fe7a191015c7f72159e432d62e427
Ashish <ashishdhomne@hotmail.com>
2022-12-03 13:29:54 +0530

    second programs

--- src/Program1.java
+++ src/Program1.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Program1 {
+public class Program1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
 }
---
+++ src/Program2_Variables.java
@@ -0,0 +1,10 @@
+public class Program2_Variables
+{
+
+        public static void main(String[] args)
+        {
+            int data=50;//instance variable
+            System.out.println(data);
+        }
+ }
+
