272c8764992163508609f10e0a5d72b68e83efd4
Ashish <ashishdhomne@hotmail.com>
2022-12-03 13:39:11 +0530

    third program

---
+++ src/Program3_if.java
@@ -0,0 +1,15 @@
+public class Program3_if
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, Ashish");
+
+        boolean isAlien=false;
+        if (isAlien==false)
+        {
+            System.out.println("It is not an alien");
+            System.out.println("I am scared of alien");
+        }
+
+    }
+}
